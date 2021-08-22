20d867e77df9af90c4f7fd67d48fd09196866e72
Tianrui Wei <tianrui-wei@outlook.com>
2021-08-22 17:35:55 +0000

    [feat] successful migen genenration
    Signed-off-by: Tianrui Wei <tianrui-wei@outlook.com>

--- litex/soc/cores/cpu/openpiton/core.py
+++ litex/soc/cores/cpu/openpiton/core.py
@@ -24,14 +24,14 @@ class OpenPitonRV64(CPU):
     name = "openpiton"
     human_name = "OpenPiton RISC-V 64Bit SoC"
     variants = CPU_VARIANTS
-    data_width = 512
-    addr_width = 64
+    data_width = 64
+    addr_width = 32
     endianness = "little"
     gcc_triple = CPU_GCC_TRIPLE_RISCV64
     linker_output_format = "elf64-littleriscv"
     nop = "nop"
     # FIXME: is this correct?
-    io_regions = {0x80000000: 0x80000000}  # Origin, Length.
+    io_regions = {0x00000000: 0x80000000}  # Origin, Length.
 
     # GCC Flags.
     @property
@@ -40,6 +40,15 @@ class OpenPitonRV64(CPU):
         flags += "-mabi=lp64 "
         flags += "-D__openpitonrv64__ "
         return flags
+
+    @property
+    def mem_map(self):
+        # Rocket reserves the first 256Mbytes for internal use, so we must change default mem_map.
+        return {
+            "csr"      : 0x12000000,
+            "main_ram" : 0x80000000,
+        }
+
 
     def __init__(self, platform, variant="standard"):
         self.platform = platform
@@ -51,8 +60,10 @@ class OpenPitonRV64(CPU):
         self.mem_axi = mem_axi = axi.AXIInterface(
             data_width=self.data_width, address_width=self.addr_width, id_width=4)
 
+        self.mem_wb = mem_wb = wishbone.Interface(data_width=self.data_width, adr_width=32-log2_int(self.data_width//8))
+        
         # Peripheral buses (Connected to main SoC's bus).
-        self.periph_buses = [mem_axi]
+        self.periph_buses = [mem_wb]
         # Memory buses (Connected directly to LiteDRAM).
         self.memory_buses = []
 
@@ -76,8 +87,8 @@ class OpenPitonRV64(CPU):
             o_m_axi_awcache=mem_axi.aw.cache,
             o_m_axi_awprot=mem_axi.aw.prot,
             o_m_axi_awqos=mem_axi.aw.qos,
-            o_m_axi_awregion=mem_axi.aw.region,
-            o_m_axi_awuser=mem_axi.aw.user,
+            # o_m_axi_awregion=mem_axi.aw.region,
+            #o_m_axi_awuser=mem_axi.aw.user,
             o_m_axi_awvalid=mem_axi.aw.valid,
             i_m_axi_awready=mem_axi.aw.ready,
 
@@ -85,7 +96,7 @@ class OpenPitonRV64(CPU):
             o_m_axi_wdata=mem_axi.w.data,
             o_m_axi_wstrb=mem_axi.w.strb,
             o_m_axi_wlast=mem_axi.w.last,
-            o_m_axi_wuser=mem_axi.w.user,
+            #o_m_axi_wuser=mem_axi.w.user,
             o_m_axi_wvalid=mem_axi.w.valid,
             i_m_axi_wready=mem_axi.w.ready,
 
@@ -98,8 +109,8 @@ class OpenPitonRV64(CPU):
             o_m_axi_arcache=mem_axi.ar.cache,
             o_m_axi_arprot=mem_axi.ar.prot,
             o_m_axi_arqos=mem_axi.ar.qos,
-            o_m_axi_arregion=mem_axi.ar.region,
-            o_m_axi_aruser=mem_axi.ar.user,
+#            o_m_axi_arregion=mem_axi.ar.region,
+            #o_m_axi_aruser=mem_axi.ar.user,
             o_m_axi_arvalid=mem_axi.ar.valid,
             i_m_axi_arready=mem_axi.ar.ready,
 
@@ -107,13 +118,13 @@ class OpenPitonRV64(CPU):
             i_m_axi_rdata=mem_axi.r.data,
             i_m_axi_rresp=mem_axi.r.resp,
             i_m_axi_rlast=mem_axi.r.last,
-            i_m_axi_ruser=mem_axi.r.user,
+            #i_m_axi_ruser=mem_axi.r.user,
             i_m_axi_rvalid=mem_axi.r.valid,
             o_m_axi_rready=mem_axi.r.ready,
 
             i_m_axi_bid=mem_axi.b.id,
             i_m_axi_bresp=mem_axi.b.resp,
-            i_m_axi_buser=mem_axi.b.user,
+            #i_m_axi_buser=mem_axi.b.user,
             i_m_axi_bvalid=mem_axi.b.valid,
             o_m_axi_bready=mem_axi.b.ready,
 
@@ -126,8 +137,6 @@ class OpenPitonRV64(CPU):
 
         # Adapt axi to wishbone
         # ----------------------------------
-        self.mem_wb = mem_wb = wishbone.Interface(
-            data_width=self.data_width, adr_width=32-log2_int(self.data_width//8))
 
         bus_a2w = ResetInserter()(axi.AXI2Wishbone(mem_axi, mem_wb, base_address=0))
         # Note: Must be reset with the CPU.
@@ -136,7 +145,7 @@ class OpenPitonRV64(CPU):
 
         # Add Verilog sources.
         # --------------------
-        self.add_sources(platform, variant)
+        self.add_sources(platform)
 
     def set_reset_address(self, reset_address):
         assert not hasattr(self, "reset_address")
